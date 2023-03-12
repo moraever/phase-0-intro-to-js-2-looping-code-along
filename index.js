22800813647c240bd90c1eb163cc9a39b9d1664f
Ever <ever.m96@hotmail.com>
2023-03-12 19:44:22 -0400

    Build a for and while loop.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, value) {
+    let messageNames = []
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "Surpise");
+
+function countDown(n) {
+    for (let i = 10; i >= 0; i--) {
+        console.log(i);
+    }
+}
